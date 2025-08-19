b9ac4b2021a0adc68db007b1a1e31981ff78d88c
Guillermo <guillermo.dv@egmail.com>
2025-08-19 12:24:14 -0300

    fix: product price

--- data/products.js
+++ data/products.js
@@ -2,7 +2,7 @@ export const products = [
   {
     id: "1",
     name: "CORVEN MILANO 150",
-    price: 49.99,
+    price: 1600000,
     image: "/corven.jpg",
     description: "4000km, bateria nueva, se usa todos los dias."
   }

baa764f398f61f00f907f34c142835d71e7ef0fc
Guillermo <guillermo.dv@egmail.com>
2025-08-19 12:07:03 -0300

    fix: add corven item

--- data/products.js
+++ data/products.js
@@ -1,9 +1,9 @@
 export const products = [
   {
     id: "1",
-    name: "Auriculares Inalámbricos",
+    name: "CORVEN MILANO 150",
     price: 49.99,
-    image: "/headphones.jpg",
-    description: "Sonido nítido, cancelación de ruido pasiva y batería para todo el día."
+    image: "/corven.jpg",
+    description: "4000km, bateria nueva, se usa todos los dias."
   }
 ];

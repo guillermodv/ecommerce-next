f539377dc4b26bfcf69597e4e547b3cd68c1e825
Guillermo A. Del Vecchio <guillermo.dv@gmail.com>
2025-08-27 12:46:49 -0300

    feat: add available

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -8,7 +8,12 @@ export default function ProductCard({ product }) {
   return (
     <div className="card p-4 flex flex-col">
       <Link href={`/product/${product.id}`} className="block">
-        <div className="h-48">
+        <div className="h-48 relative">
+          {product.status !== 'available' && (
+            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+              <span className="text-white text-2xl font-bold">Vendido</span>
+            </div>
+          )}
           <img
             src={product.images[0]}
             alt={product.name}
@@ -20,22 +25,24 @@ export default function ProductCard({ product }) {
       <p className="text-grey-500 text-sm flex-1">{product.description}</p>
       <div className="mt-3 flex items-center justify-between">
         <span className="text-xl font-bold">${product.price.toFixed(2)}</span>
-        <div className="flex gap-2">
-          <a
-            href={`${MESSAGE_DELIVERY}:%20${product.name}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="px-3 py-2 border border-slate-700 hover:bg-green-800 flex justify-center"
-          >
-            <img src="/whatsapp.svg" alt="WhatsApp" className="w-6 h-6" />
-          </a>
-          <button
-            className="px-3 py-2 rounded-xl border border-slate-700 hover:bg-slate-800 hover:text-red-500"
-            onClick={() => addToFavorites(product, 1)}
-          >
-            {FAVORITES_PAGE.ADD_TO_FAVORITES}
-          </button>
-        </div>
+        {product.status === 'available' && (
+          <div className="flex gap-2">
+            <a
+              href={`${MESSAGE_DELIVERY}:%20${product.name}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-3 py-2 border border-slate-700 hover:bg-green-800 flex justify-center"
+            >
+              <img src="/whatsapp.svg" alt="WhatsApp" className="w-6 h-6" />
+            </a>
+            <button
+              className="px-3 py-2 rounded-xl border border-slate-700 hover:bg-slate-800 hover:text-red-500"
+              onClick={() => addToFavorites(product, 1)}
+            >
+              {FAVORITES_PAGE.ADD_TO_FAVORITES}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- data/products.js
+++ data/products.js
@@ -4,13 +4,23 @@ export const products = [
     name: "CORVEN MILANO 150",
     price: 1600000,
     images: ["/corven.jpg", "/corven.jpg", "/corven.jpg"],
-    description: "4000km, bateria nueva, se usa todos los dias."
+    description: "4000km, bateria nueva, se usa todos los dias.",
+    status: "available"
   },
   {
     id: "2",
     name: "Iphone 12 pro",
     price: 400,
     images: ["/iphone12.png"],
-    description: "Precio en USD, 82% bateria perfecto estado."
+    description: "Precio en USD, 82% bateria perfecto estado.",
+    status: "available"
+  },
+  {
+    id: "2",
+    name: "Iphone 12 pro",
+    price: 400,
+    images: ["/iphone12.png"],
+    description: "Precio en USD, 82% bateria perfecto estado.",
+    status: "sold"
   },
 ];
